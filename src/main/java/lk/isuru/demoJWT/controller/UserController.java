70fd1e00bfa9dc47bef56d14943f98fd0679ef65
Isuru Sampath <isurusampathcs@gmail.com>
2023-10-10 14:54:11 +0530

    Add unsecured endpoint for testing

--- src/main/java/lk/isuru/demoJWT/controller/UserController.java
+++ src/main/java/lk/isuru/demoJWT/controller/UserController.java
@@ -1,5 +1,10 @@
 package lk.isuru.demoJWT.controller;
 
+import lk.isuru.demoJWT.service.JwtService;
+import lk.isuru.demoJWT.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class UserController
 {
+    @Autowired
+    private UserInfoService service;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome this endpoint is not secure";
+    }
+    
 }
