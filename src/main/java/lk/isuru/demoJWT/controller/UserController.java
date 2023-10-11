1b938cb3d2cbf7f191936ce4ad8ad6ccabb85f9b
Isuru Sampath <isurusampathcs@gmail.com>
2023-10-11 10:22:42 +0530

    Define secure and unsecure endpoints for testing

--- src/main/java/lk/isuru/demoJWT/controller/UserController.java
+++ src/main/java/lk/isuru/demoJWT/controller/UserController.java
@@ -1,12 +1,16 @@
 package lk.isuru.demoJWT.controller;
 
+import lk.isuru.demoJWT.entity.AuthRequest;
+import lk.isuru.demoJWT.entity.UserInfo;
 import lk.isuru.demoJWT.service.JwtService;
 import lk.isuru.demoJWT.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,5 +29,31 @@ public class UserController
     public String welcome(){
         return "Welcome this endpoint is not secure";
     }
-    
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/user/userProfile")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String userProfile() {
+        return "Welcome to User Profile";
+    }
+
+    @GetMapping("/admin/adminProfile")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminProfile() {
+        return "Welcome to Admin Profile";
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
 }
